906ce58fc1643bfe8580c9b22a75738f3213915e
TheRoofyDude <abrahamphilip47@gmail.com>
2022-09-11 17:31:22 +0530

    Configure React app for deployment to GitHub Pages

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,16 +1,16 @@
 import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
-import { FaFire, FaPoo } from 'react-icons/fa';
+import { FaDiscord, FaDigitalOcean } from 'react-icons/fa';
 
 const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
                   bg-white dark:bg-gray-900 shadow-lg">
                     
-        <SideBarIcon icon={<FaFire size="28" />} />
+        <SideBarIcon icon={<FaDiscord size="28" />} />
         <Divider />
         <SideBarIcon icon={<BsPlus size="32" />} />
         <SideBarIcon icon={<BsFillLightningFill size="20" />} />
-        <SideBarIcon icon={<FaPoo size="20" />} />
+        <SideBarIcon icon={<FaDigitalOcean size="20" />} />
         <Divider />
         <SideBarIcon icon={<BsGearFill size="22" />} />
     </div>
